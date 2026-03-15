0e5469fd74e0ae8abc90d32bd0748babb9559a24
paribhoge123 <paribhoge@gmail.com>
2026-03-15 15:27:55 +0530

    learned about seperation of models

---
+++ models/owner-model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const ownerSchema = mongoose.Schema({
+    fullname : {
+        type : String,
+        minLength : 3,
+        trim : true,  
+    },
+    email  : String,
+    password : String,
+    picture : String,
+    gstin : String,
+});
+
+ mongoose.exports  = mongoose.model("owner", ownerSchema);
