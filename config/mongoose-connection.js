05456a947056ca3f8b37932f2bf245513c689b54
paribhoge123 <paribhoge@gmail.com>
2026-05-20 01:10:49 +0530

    fixed mongodb connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,30 @@
- const mongoose = require('mongoose');
+//  const mongoose = require('mongoose');
+// const debug = require("debug")("application:database");
+// const config = require("config");
+
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}/firststep`, )
+// .then(function(){
+//     debug("connected to database");
+// })
+// .catch(function(err){
+//     debug(err);
+// });
+
+// module.exports = mongoose.connection;
+
+const mongoose = require("mongoose");
 const debug = require("debug")("application:database");
-const config = require("config");
 
 mongoose
-.connect(`${config.get("MONGODB_URI")}/firststep`, )
+.connect(`${process.env.MONGODB_URI}/firststep`)
 .then(function(){
     debug("connected to database");
+    console.log("MongoDB Connected");
 })
 .catch(function(err){
     debug(err);
+    console.log(err);
 });
 
 module.exports = mongoose.connection;
